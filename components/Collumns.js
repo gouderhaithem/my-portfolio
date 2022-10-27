c58825f0d3e45d8911fbe68e30e41319e0c28126
gouder hicham <gouderhicham@gmail.com>
2022-10-27 14:30:37 +0100

    add skills boiler plate

---
+++ components/Collumns.js
@@ -0,0 +1,39 @@
+const Collumns = () => {
+  return (
+    <section>
+      <div className="col">
+        <img />
+        <h3>Title</h3>
+        <p>descreption</p>
+        <span></span>
+        <p></p>
+        <span></span>
+        <div>
+          {/* skills  */}
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+        </div>
+      </div>
+      <div className="col">
+        <img />
+        <h3>Title</h3>
+        <p>descreption</p>
+        <span></span>
+        <p></p>
+        <span></span>
+        <div>
+          {/* skills  */}
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Collumns;
--- pages/index.js
+++ pages/index.js
@@ -28,6 +28,17 @@ export default function Home() {
             alt="hero-image-logo"
           />
         </header>
+        <section className={styles.main_section}>
+          <h1>Hi, I'm Matt. Nice to meet you.</h1>
+          <p>
+            Since beginning my journey as a freelance designer over 11 years
+            ago, I've done remote work for agencies, consulted for startups, and
+            collaborated with talented people to create digital products for
+            both business and consumer use. I'm quietly confident, naturally
+            curious, and perpetually working on improving my chops one design
+            problem at a time.
+          </p>
+        </section>
       </main>
     </div>
   );
