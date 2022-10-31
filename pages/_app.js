7c2476ee7e0c04eb65f64c93865bb223b2e8a5f7
gouder hicham <gouderhicham@gmail.com>
2022-10-31 10:29:40 +0100

    added google meta verification tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
 import "../styles/globals.css";
 import Nav from "../components/Nav";
 import "aos/dist/aos.css";
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="wKq_AE2WgOioSjPgF6wHt6HNULSiyFpOgMrQ0hmL08k"
+        />
+      </Head>
       <Nav />
       <Component {...pageProps} />
     </>
