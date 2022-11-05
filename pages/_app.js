8de83f78cdcb257ac5ec21bad3550813ea9d3726
gouder hicham <gouderhicham@gmail.com>
2022-11-05 17:06:12 +0100

    gege

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,10 @@ function MyApp({ Component, pageProps }) {
           name="google-site-verification"
           content="wKq_AE2WgOioSjPgF6wHt6HNULSiyFpOgMrQ0hmL08k"
         />
+        <meta
+          name="google-site-verification"
+          content="xnBqd2s9vl_rUtHUk6UXbusGQFgww8wNiWVH0FwornQ"
+        />
       </Head>
       <Nav />
       <Component {...pageProps} />
